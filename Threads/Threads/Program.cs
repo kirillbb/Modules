97c185cc0f2a1e5943dfb60a5231b777c58060eb
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-07-28 11:01:39 +0300

    initial commit

---
+++ Threads/Threads/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+Random r = new Random();
+
+decimal[] arr = Enumerable.Range(0, 1000000).Select(_ => (decimal)r.NextDouble()).ToArray();
+decimal sum = 0;
+
+var sw = Stopwatch.StartNew();
+
+foreach (var item in arr)
+{
+    sum += item;
+}
+
+sw.Stop();
+Console.WriteLine(sw.ElapsedMilliseconds);
+
+//default 25-26
+//best ___
