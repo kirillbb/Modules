3079036e888038ab952150a2e24d7fcee699677e
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-07-31 15:35:26 +0300

    search authors is work now

---
+++ BookParse/BookManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookParse
+{
+    internal class BookManager
+    {
+        public List<Book> AddBooksFromFile(string path)
+        {
+            Serializer serializer = new Serializer();
+            List<Book> bookList = serializer.BookDeserialize(path);
+            return bookList;
+        }
+
+        public Book FindLastPublishedBook(List<Book> bookList)
+        {
+            Book lastPublishedBook = (from book in bookList
+                                      orderby book.Date
+                                      select book).LastOrDefault();
+
+            return lastPublishedBook;
+        }
+
+        public List<Book> FindBooksBetweenYears(int startYear, int endYear, List<Book> bookList)
+        {
+            List<Book> foundedBooks = new List<Book>();
+            if (bookList.Count == 0)
+                throw new Exception(message: "Books list are empty");
+
+            foreach (var book in bookList)
+            {
+                if (book.Date.Year >= startYear && book.Date.Year <= endYear)
+                    foundedBooks.Add(book);
+            }
+
+            if (foundedBooks.Count == 0)
+                throw new Exception(message:"No books for this time range");
+
+            return foundedBooks; 
+        }
+
+
+        public Book FindBookByName(string name, List<Book> bookList)
+        {
+            if (bookList.Count == 0)
+                throw new Exception(message: "Books list are empty");
+
+            foreach (var book in bookList)
+            {
+                if (book.BookName.ToLower() == name.ToLower())
+                {
+                    return book;
+                }
+            }
+            throw new Exception(message: "Book not found");
+        }
+
+        public string FindAuthors(List<Book> bookList)
+        {
+            if (bookList.Count == 0)
+                throw new Exception(message: "Books list are empty");
+
+            var authorsList = from b in bookList
+                              orderby b.Author
+                              select b.Author;
+
+            string authorsString = "";
+            foreach (var author in authorsList)
+            {
+                if (!authorsString.Contains(author))
+                {
+                    if (author != authorsList.Last())
+                    {
+                        authorsString += $"{author}, ";
+                    }
+                    else
+                    {
+                        authorsString += $"{author}.";
+                    }
+                }
+            }
+            return authorsString;
+        }
+
+        public void ShowBooks(List<Book> bookList)
+        {
+            if (bookList.Count == 0)
+                throw new Exception(message: "Books list are empty");
+
+            int i = 1;
+            foreach (var item in bookList)
+            {
+                Console.WriteLine(item.ToString());
+
+                if (i == 3)
+                {
+                    Console.WriteLine("Press any button to go to the next page...");
+                    Console.ReadKey();
+                    Console.Clear();
+                    i = 1;
+                }
+                i++;
+            }
+        }
+    }
+}
--- BookParse/Program.cs
+++ BookParse/Program.cs
@@ -6,8 +6,9 @@ namespace BookParse
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the information about the book:");
-            Console.WriteLine("Example:  Е. Таненбаум – Сучасні операційні системи, 1365 (12.05.2013).pdf");
+            string path = @"books.json";
+            //Console.WriteLine("Enter the information about the book:");
+            //Console.WriteLine("Example:  Е. Таненбаум – Сучасні операційні системи, 1365 (12.05.2013).pdf");
 
             //BookCreator bookCreator = new BookCreator();
 
@@ -16,27 +17,31 @@ namespace BookParse
 
             //Serializer serializer = new Serializer();
             //if (book != null)
-            //    serializer.BookSerialize(book);
+            //    serializer.BookSerialize(book, path);
 
-            View bookView = new View();
-            var bookList = bookView.AddBooksFromFile(@"books.json");
+            BookManager bookManager = new BookManager();
+            var bookList = bookManager.AddBooksFromFile(path);
 
             try
             {
-                //bookView.ShowBooks(bookList);
+                //bookManager.ShowBooks(bookList);
 
-                //var lastPublishedBook = bookView.FindLastPublishedBook(bookList);
+                //var lastPublishedBook = bookManager.FindLastPublishedBook(bookList);
                 //Console.WriteLine("Last published book is:");
                 //Console.WriteLine(lastPublishedBook.ToString());
 
 
-                //List<Book> booksYears = bookView.FindBooksBetweenYears(1969, 1970, bookList);
-                //bookView.ShowBooks(booksYears);
+                //List<Book> booksYears = bookManager.FindBooksBetweenYears(1969, 1970, bookList);
+                //bookManager.ShowBooks(booksYears);
 
-                Book foundedBook = bookView.FindBookByName("Born", bookList);
-                Console.WriteLine(foundedBook.ToString());
-                foundedBook = bookView.FindBookByName("Bosdfsdfrn", bookList);
-                Console.WriteLine(foundedBook.ToString());
+                //Book foundedBook = bookManager.FindBookByName("Born", bookList);
+                //Console.WriteLine(foundedBook.ToString());
+                //foundedBook = bookManager.FindBookByName("Bosdfsdfrn", bookList);
+                //Console.WriteLine(foundedBook.ToString());
+
+                //Console.WriteLine(bookManager.FindAuthors(bookList));
+
+
             }
             catch (Exception ex)
             {
