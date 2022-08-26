830a770338e1ca1690111c863928d783e542c171
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-08-26 17:51:47 +0300

    rework 9th part

--- JavaScript/js/array-join-demo.js
+++ JavaScript/js/array-join-demo.js
@@ -1,8 +1,10 @@
 function Test(){
- var arr = ["Ivanov", "Ivan", "Ivanovich"];
- var arrSeparator = "***";
- var arrStr = combine(arr, arrSeparator);
- document.getElementById("convertedArr").innerHTML = arrStr;
+  var str = document.getElementById("txtInput").value;
+  var sep = document.getElementById("separatorInput").value;
+  var arr = str.split(sep);
+  var arrSeparator = "***";
+  var arrStr = combine(arr, arrSeparator);
+  document.getElementById("convertedArr").innerHTML = arrStr;
 }
 function combine(Arr, ArrSeparator){
   return Arr.join(ArrSeparator);
