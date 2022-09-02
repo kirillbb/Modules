8a8dc53b797910c59437bacf34620cf0a2a7423b
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-09-02 20:54:34 +0300

    asp start

---
+++ AspStart/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspStart.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
