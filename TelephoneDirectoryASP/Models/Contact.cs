2dc979f914e6ebce5ab9bf15587246203afb5b3e
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-09-13 12:09:07 +0300

    add model contact, contactController, db context, migrations

---
+++ TelephoneDirectoryASP/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace TelephoneDirectoryASP.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+    }
+}
