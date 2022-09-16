4532adac16925fd27f9509177dfcab6121c25e36
Kirill Sokolov <kirill.sokolov.work@gmail.com>
2022-09-16 14:04:45 +0300

    add models, add get, post methods

---
+++ BooksWebApi/Controllers/BooksController.cs
@@ -0,0 +1,39 @@
+﻿using BooksWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : Controller
+    {
+        private readonly BooksAppContext context;
+
+        public BooksController(BooksAppContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetBooksAsync()
+        {
+            return Ok(await context.Books.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddContactAsync(AddBookRequest request)
+        {
+            var book = new Book()
+            {
+                Id = new Guid(),
+                Title = request.Title,
+                Author = request.Author
+            };
+
+            await context.Books.AddAsync(book);
+            await context.SaveChangesAsync();
+            return Ok(book);
+        }
+    }
+}
--- BooksWebApi/Program.cs
+++ BooksWebApi/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using BooksWebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<BooksAppContext>(opt =>
+opt.UseInMemoryDatabase("BooksDB"));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -12,6 +19,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage(); // ??
     app.UseSwagger();
     app.UseSwaggerUI();
 }
